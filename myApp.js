249e8a0b45fd9cafcf81f259d5ad62f43d9708fd
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 00:47:26 +0100

    add helmet.xssFilter()for MIME protection

--- myApp.js
+++ myApp.js
@@ -2,8 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}))
-app.use(helmet.xssFilter());  
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 
 
