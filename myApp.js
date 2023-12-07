350b1f93d6ade19766c7c85880a888905c6eaa40
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 13:25:39 +0100

    Refactor helmet parent for simplicity and removed the unnecessary

--- myApp.js
+++ myApp.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-const timeInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives :{
-    defaultSrc:["'self'"],
-    scriptSrc:["'self'",'trusted-cdn.com']
-    }
-  }
-));
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// const timeInSeconds = 90*24*60*60;
+// app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({
+//   directives :{
+//     defaultSrc:["'self'"],
+//     scriptSrc:["'self'",'trusted-cdn.com']
+//     }
+//   }
+// ));
 app.use(helmet({
   
   contentSecurityPolicy:{
