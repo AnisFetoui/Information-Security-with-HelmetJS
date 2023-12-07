ffad4ffb130e0d4272d38fc281e6270d6471319b
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 12:57:49 +0100

    add content security policy

--- myApp.js
+++ myApp.js
@@ -10,6 +10,14 @@ const timeInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives :{
+    defaultSrc:["'self'"],
+    scriptSrc:["'self'",'trusted-cdn.com']
+    }
+  }
+));
+
 
 
 
