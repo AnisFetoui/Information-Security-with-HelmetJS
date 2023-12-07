f39d950ac6db40ce321076493b358bae1607ba7c
AnisFetoui <anis.fetoui@esprit.tn>
2023-12-07 11:42:17 +0100

    add helmet dns prefetech control

--- myApp.js
+++ myApp.js
@@ -8,6 +8,8 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 const timeInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+
 
 
 
